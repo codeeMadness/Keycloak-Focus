342d9563d76375050fc07fc619789319274e4a89
codee.madness <codee.madness@gmail.com>
2024-07-03 08:42:44 +0700

    create client

---
+++ my-client/assets/js/myLogic.js
@@ -0,0 +1,40 @@
+const keycloak = new Keycloak({
+  url: "http://localhost:8080",
+  realm: "hang",
+  clientId: "mydemoclient",
+});
+
+function initKeycloak() {
+    keycloak.init({onLoad: 'login-required'}).then(function() {
+        constructTableRows(keycloak.idTokenParsed);
+        pasteToken(keycloak.token);
+    }).catch(function() {
+        console.log('failed to initialize');
+    });
+}
+
+function constructTableRows(keycloakToken) {
+    document.getElementById('row-username').innerHTML = keycloakToken.preferred_username;
+    document.getElementById('row-firstName').innerHTML = keycloakToken.given_name;
+    document.getElementById('row-lastName').innerHTML = keycloakToken.family_name;
+    document.getElementById('row-name').innerHTML = keycloakToken.name;
+    document.getElementById('row-email').innerHTML = keycloakToken.email;
+    document.getElementById('row-verify').innerHTML = keycloakToken["verify-document"];
+}
+
+function pasteToken(token){
+    document.getElementById('ta-token').value = token;
+    document.getElementById('ta-refreshToken').value = keycloak.refreshToken;
+}
+
+var refreshToken = function() {
+    keycloak.updateToken(-1)
+    .then(function(){
+        document.getElementById('ta-token').value = keycloak.token;
+        document.getElementById('ta-refreshToken').value = keycloak.refreshToken;
+    });
+}
+
+var logout = function() {
+    keycloak.logout({"redirectUri":"http://localhost:5500/logout.html"});
+}
