7bfed65274d85ccae3656847cd26d7b90c8cd87c
codee.madness <codee.madness@gmail.com>
2024-07-10 13:42:54 +0700

    update

--- my-client/assets/js/myLogic.js
+++ my-client/assets/js/myLogic.js
@@ -38,3 +38,23 @@ var refreshToken = function() {
 var logout = function() {
     keycloak.logout({"redirectUri":"http://localhost:5500/logout.html"});
 }
+
+var checkAccess = async function() {
+    const url = "https://9dry6fle9j.execute-api.ap-southeast-1.amazonaws.com/dev/demo-resource";
+    try {
+        const response = await fetch(url, {
+            headers: {
+                "auth-token": keycloak.token,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
