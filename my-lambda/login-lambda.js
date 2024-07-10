d3541cfdc496e71c7d57f533399a68ee03d31b6a
codeeMadness <codee.madness@gmail.com>
2024-07-10 23:35:46 +0700

    update

--- my-lambda/login-lambda.js
+++ my-lambda/login-lambda.js
@@ -1,23 +1,31 @@
 const axios = require("axios");
 
-const data = {
+const handler = async () => {
+  const data = {
     "grant_type": "password",
     "client_id": "admin-cli",
     "scope": "openid",
     "username": "hang.nguyen",
     "password": "P@assword"
-}
+  }
 
-const url = "https://keycloak.sbot.pro/realms/master/protocol/openid-connect/token";
+  const url = "https://keycloak.sbot.pro/realms/master/protocol/openid-connect/token";
 
-axios.post(url, data, {
+  try {
+    // const apiURL = "https://catfact.ninja/fact"
+    const response = await axios.post(url, data, {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         }
     })
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    const body = response.data
+    console.log(JSON.stringify(body))
+    
+  } catch (error) {
+    console.log("Error")
+  }
+}
+
+handler();
+
+
