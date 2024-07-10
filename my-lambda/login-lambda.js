0785b1fa78084f45ab90db11cbd32251240bca7d
codee.madness <codee.madness@gmail.com>
2024-07-10 16:44:11 +0700

    update

--- my-lambda/login-lambda.js
+++ my-lambda/login-lambda.js
@@ -1,12 +1,4 @@
 const axios = require('axios');
-var FormData = require('form-data');
-
-const formData = new FormData();
-formData.append('grant_type', 'password')
-formData.append('client_id', 'admin-cli')
-formData.append('scope', 'openid')
-formData.append('username', 'hang.nguyen')
-formData.append('password', 'P@assword')
 
 const data = {
     'grant_type': 'password',
@@ -24,7 +16,6 @@ axios.post(url, data, {
         }
     })
   .then((response) => {
-    debugger;
     console.log(response.data);
   })
   .catch((error) => {
